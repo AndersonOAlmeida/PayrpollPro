6abb50f763206031836e59b0e746a9c02b4c0e00
Anderson Almeida <116223172+AndersonOAlmeida@users.noreply.github.com>
2023-11-12 19:52:23 -0300

    testes integração  bd

---
+++ config.php
@@ -0,0 +1,16 @@
+<?
+    $dbHost = 'LocalHost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName = 'regPonto';
+
+    $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+
+    if ($conexao->connect_errno){
+        echo "Erro";
+    } else {
+        echo "Conexão Bem  Sucedida!";
+    }
+
+
+?>
